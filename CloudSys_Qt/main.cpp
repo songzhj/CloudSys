f58ee9e983c75a068f69946ada7ee34fbb4f7a7d
songzhj <songzhj@aliyun.com>
2015-08-15 15:19:50 +0800

    登录界面
    登录界面重新布局

--- CloudSys_Qt/main.cpp
+++ CloudSys_Qt/main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "login.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    Login login;
+    login.show();
 
     return a.exec();
 }
